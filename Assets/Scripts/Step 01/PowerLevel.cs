80a50f0503c5dfedbcb7e9f42fb12d9ce14c8a35
ChesterRocela <107157644+ChesterRocela@users.noreply.github.com>
2022-07-18 21:36:07 +1000

    18/7

--- Assets/Scripts/Step 01/PowerLevel.cs
+++ Assets/Scripts/Step 01/PowerLevel.cs
@@ -34,14 +34,17 @@ public class PowerLevel : MonoBehaviour
     public float ReturnChanceToWin(int myPowerLevel, int opponentPowerLevel)
     {
         // let's first calculate the total power level overall.
+       
         int totalPower = myPowerLevel + opponentPowerLevel;
 
         // Then let's then do a fraction of my power level and the overall power level.
 
         // This will give us a decimal number, i.e. 3/4 will give us 0.75 we probably want to turn that into the percentage value.
+        
         float myChanceToWin = (float)myPowerLevel / (float)totalPower * 100;
 
         Debug.Log(myChanceToWin + "");
+
         return myChanceToWin; // Instead of returning 0 here we probably want to return our percentage chance to win.
     }
 
